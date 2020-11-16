4b0800392d47d47a1455ba0d4948d5fa77e2db26
MrDuffs <chulkov.i@yandex.ru>
2020-11-16 14:42:03 +0300

    Added sidebar menu for small screens

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,18 +2,23 @@ import React from 'react'
 import '../assets/Home.css'
 import NavBar from './NavBar'
 import { VscFoldDown } from 'react-icons/vsc'
+import { FaInfinity } from 'react-icons/fa'
 
 export default function Home()  {
     return (
         <div className='home-wrapper' >
             <div className='home'>
+                <div className='home-sidebar'>      
+                    <div className='home-sidebar-logo'> <FaInfinity size={'1.3em'}  strokeWidth={0.5} /> </div>
+                    <p className='home-sidebar-text'>menu</p>
+                </div>
                 <div className='home-menu'>
                     <NavBar menuColor='navbar' />
                     <span className='home-line'></span>
                 </div>
                 <div className='home-scrolling-content'>
                     <p className='home-scroll-text'>scroll down to see more</p>
-                    <p className='home-arrow'> <VscFoldDown size={'2em'}  strokeWidth={0.5} /> </p>
+                    <div className='home-arrow'> <VscFoldDown size={'2em'}  strokeWidth={0.5} /> </div>
                 </div> 
             </div>
         </div>
